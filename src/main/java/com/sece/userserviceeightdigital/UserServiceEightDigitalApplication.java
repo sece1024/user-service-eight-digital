38fc89a6b1db0009fe5b3ab2911aa8b0f42213b4
gaoxiang.huang <577174012@qq.com>
2022-09-09 15:07:05 +0800

    chore: init with SpringBoot

---
+++ src/main/java/com/sece/userserviceeightdigital/UserServiceEightDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.sece.userserviceeightdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceEightDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceEightDigitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sece/userserviceeightdigital/UserServiceEightDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sece.userserviceeightdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceEightDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
